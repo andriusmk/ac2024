69e768c0de09b6f6e8a5664d1ef9fae347593cc4
Andrius Mažeikis <andriusmk@gmail.com>
2024-12-03 06:39:22 +0200

    Kind of optmization

--- day02/doit2.py
+++ day02/doit2.py
@@ -2,6 +2,8 @@ from typing import Iterable
 import sys
 from aoc_shared.utils import distance, make_file_parser, pipe
 from day02_shared import pair_adjacent, parse_int_tuples,  Item, InputData
+from pprint import pprint
+import itertools
 
 def main(argv: list[str]):
     parse_input = make_file_parser(parse_int_tuples)
@@ -9,7 +11,21 @@ def main(argv: list[str]):
 
 
 def process(input_data: InputData) -> int:
-    return sum(1 for report in input_data if is_safe(report))
+    return sum(1 for report in input_data if is_any_safe(report))
+
+
+def is_any_safe(report: Item) -> bool:
+    combinations = itertools.chain((report,), reduced_sets(report))
+    return any(is_safe(r) for r in combinations)
+
+def reduced_sets(report: Item) -> Iterable[Item]:
+    removed_value = report[0]
+    values = list(report[1:])
+    for index in range(len(values)):
+        yield tuple(values)
+        values[index], removed_value = removed_value, values[index]
+    yield tuple(values)
+
 
 def is_safe(report: Item) -> bool:
     pairs = tuple(pair_adjacent(report))
