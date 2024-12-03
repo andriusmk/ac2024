efb797f4ccec6163d62899ad5608f671774cabb0
Andrius Mažeikis <andriusmk@gmail.com>
2024-12-03 19:55:59 +0200

    Cleanup

--- day03/day03_shared.py
+++ day03/day03_shared.py
@@ -1,5 +1,4 @@
 from typing import Iterator
-import operator
 import re
 
 mul_statement = re.compile(r"mul\((\d{1,3}+),(\d{1,3}+)\)")
@@ -7,5 +6,5 @@ mul_statement = re.compile(r"mul\((\d{1,3}+),(\d{1,3}+)\)")
 def extract_products(input_data: str) -> Iterator[int]:
     matches = re.findall(mul_statement, input_data)
     number_pairs = (map(int, pair) for pair in matches)
-    products = (operator.mul(*pair) for pair in number_pairs)
+    products = (x * y for x, y in number_pairs)
     return products
